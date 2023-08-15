1a27178a1938306b3865b4b4cc23794cf1444923
Million <millionsilishe@gmail.com>
2023-08-15 07:50:27 -0700

    Add a function that add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @num1: number
+ * @num2: number
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}

79d616ad77b95febc6827905206b3d3e7892c935
Million <millionsilishe@gmail.com>
2023-08-17 09:11:07 -0700

    Add if condition

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *
+ * print_line - Draws a straight line in the terminal
+ * @n: number of times
  */
 void print_line(int n)
 {
@@ -9,6 +10,10 @@ void print_line(int n)
 
 	while (i <= n)
 	{
+		if (n == 0)
+		{
+			_putchar(10);
+		}
 		_putchar(95);
 	i++;
 	}

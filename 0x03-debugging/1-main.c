12741aff468ce0bcd5ab4e52b9646bc73367c37d
Million <millionsilishe@gmail.com>
2023-08-18 11:15:35 -0700

    Remove line

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
 
@@ -10,9 +11,8 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+	i = 0;
 	/*
-	 * i = 0;
-	 *
 	 * while (i < 10
 	 * {
 	 * putchar(i);

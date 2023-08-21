b3706c220cfdf16d868154d23db6a43afc75b20e
Million <millionsilishe@gmail.com>
2023-08-21 12:19:38 -0700

    Add description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Updates the value it points to to 98
+ * reset_to_98 - set the integer to 98
+ * @n: a pointer the integer we want to set to 98
  *
  * Return: Nothing
  */

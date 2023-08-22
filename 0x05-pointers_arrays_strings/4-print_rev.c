8061d5d2698185539510c34835a9710677727c3d
Million <millionsilishe@gmail.com>
2023-08-22 04:55:50 -0700

    Add a function prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse followed by a new line, to stdout
+ * @s: a pointer point to a string
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	char *p = s;
+	int length, i;
+
+	while (*p != 0)
+	{
+		p++;
+	}
+	length = p - s;
+	p = s + length;
+	for (i = 0; i <= length; i++)
+	{
+		_putchar(*p);
+		p--;
+	}
+	_putchar(10);
+}

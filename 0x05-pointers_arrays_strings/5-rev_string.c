c8232f0be5012c3bd2ae2d677014303059ac40d9
Million <millionsilishe@gmail.com>
2023-08-23 01:50:51 -0700

    Update function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Prints a string, in reverse followed by a new line, to stdout
+ * @s: a pointer point to a string
+ *
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	char array = s[0];
+	int size = 0, i;
+	
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+	for (i = 0; i > size; i++)
+	{
+		size--;
+		array = s[i];
+		s[i] = s[size];
+		s[size] = array;
+	}
+}
